45ed808650c1161dbf082693ab1b6c43cebf27f2
mortiziribar-boop <m.ortiziribar@ikasle.eus>
2026-02-16 13:58:28 +0100

    Add files via upload

---
+++ funtzioenbaliotaula.py
@@ -0,0 +1,19 @@
+import math
+#erro2=math.sqrt(x)
+#erro3=math.cbrt(x)
+#esponentzialak=math.exp(x)
+#logaritmikoa=math.log(x)
+funtzioa=input("zartu zure funtzioa,adb:(3*x-5):")
+pkopurua=int(input("idatzi zenbat puntu nahi dituzun:"))
+puntuak=[]
+for y in range(pkopurua):
+    balioak=float(input("Eman x-eri balio bat:"))
+    puntuak.append(balioak)
+    
+print("\nZuk ataratako puntuak:")
+for x in puntuak:
+    try:
+        y=eval(funtzioa)
+        print(f"puntua:({x},{y})")
+    except:
+        print("Errore matematiko bat izan da.")
